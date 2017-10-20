0bc102daf725fc77af8c4e0c92554bbc1df74cf4
Never2l8 <dushka888@yahoo.com>
2017-10-20 18:00:19 -0500

    Initial commit

---
+++ models/database.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://postgres:1986@localhost:5432/postgres');
+sequelize
+    .authenticate()
+    .then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+const User = sequelize.define('user', {
+    firstName: {
+        type: Sequelize.STRING
+    },
+    lastName: {
+        type: Sequelize.STRING
+    }
+});
+
+// force: true will drop the table if it already exists
+User.sync({force: true}).then(() => {
+    // Table created
+    return User.create({
+        firstName: 'John',
+        lastName: 'Hancock'
+    });
+});
+
+
+module.exports = sequelize;
