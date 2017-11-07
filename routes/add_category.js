12a8acdcb676bf81d5aa66e6ae8765a8f0c638ca
Never2l8 <dushka888@yahoo.com>
2017-11-07 04:22:04 -0600

    add account page

--- routes/add_category.js
+++ routes/add_category.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const models  = require('../new_models');
 
 router.get('/', function(req, res, next) {
-    res.render('add_category', { title: 'Add Category' });
+    models.category.findAll({
+        attributes: ['name']
+    }).then(categories => {
+        console.log(categories);
+        res.render('add_category', {
+            title: 'Add Category',
+            categories: categories
+        });
+    });
+
 });
+
+
+router.post('/', function(req, res, next) {
+    models.category.findAll({
+        attributes: ['name']
+    }).then(categories => {
+        console.log(categories);
+        res.render('add_category', {
+            title: 'Add Category',
+            categories: categories
+        });
+    });
+    console.log('+++++++++++++++++++++++++++++++++++++++++++++++++++');
+    console.log(req.body.category_name);
+    console.log(req.body.parent_category);
+});
+
+
 
 module.exports = router;
