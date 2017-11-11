65fff7f53b0d7862291d36affcecae45e8fc7d05
Never2l8 <dushka888@yahoo.com>
2017-11-10 18:07:33 -0600

    add general route itemCard page

--- routes/itemCard.js
+++ routes/itemCard.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('valentino_floral_print_gown', { title: 'Express' });
+    res.render('itemCard', { title: 'Express' });
 });
 
 module.exports = router;
